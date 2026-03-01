7d79a190da5a5c7513d449feb64893547d71c6ee
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-03-01 16:32:09 -0300

    feat(PopupsContext.js): add context to popups

---
+++ src/contexts/PopupsContext.js
@@ -0,0 +1,10 @@
+// ------------------------------------------------------
+// Contexto que armazena informações sobre gerenciamento
+// dos popups do app (apenas handlers)
+// ------------------------------------------------------
+
+import { createContext } from 'react';
+
+const PopupsContext = createContext();
+
+export default PopupsContext;
